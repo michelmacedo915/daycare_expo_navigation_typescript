ee960983e8d8ae77fe5c47293f192c5ade7db4bf
michelmacedo915 <michelmacedo@gmail.com>
2025-12-02 13:38:25 +1100

    activities screen added

--- assets/data/activities.ts
+++ assets/data/activities.ts
@@ -1,11 +1,11 @@
 const activities = [
   {
     id: 1,
-    headline: 'Emotion Faces Matching Games',
+    headline: 'Emotion Faces Matching Games - Social and emotion learning',
     date: '25 Nov 2025',
     educator: 'Angila Sharma',
     room: '3-4 Pioneers',
-    description: "Today, we're embarking on an emotional adventure with our Faces Matching Game! It's a delightful way for children to explore feelings and understand how to recognize them in themselves and others. We'll be making funny faces, playing engaging matching games, and swapping stories about our own emotions. Prepare for a day filled with laughter, learning, and profound emotional discovery!",
+    description: "Today, we're embarking on an emotional adventure with our Faces Matching Game! It's a delightful way for children to explore feelings and understand how to recognize them in themselves and others. We'll be making funny faces, playing engaging matching games, and swapping stories about our own emotions. Prepare for a day filled with laughter, learning, and profound emotional discovery! Our activities are designed to help children develop emotional intelligence, fostering empathy and self-awareness. Through interactive play, they'll learn to identify and express a wide range of emotions, from happiness and excitement to sadness and frustration. We'll create a supportive environment where every child feels safe to share their feelings and experiences.",
   },
   {
     id: 2,
