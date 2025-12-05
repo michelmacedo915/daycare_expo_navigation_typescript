71dc5c0fe112d3d270daf248c9868583017222ac
michelmacedo915 <michelmacedo@gmail.com>
2025-12-05 16:27:14 +1100

    authentication

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -57,6 +57,7 @@ function RootLayoutNav() {
       <Stack>
         <Stack.Screen name="(admin)" options={{ headerShown: false }} />
         <Stack.Screen name="(user)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
